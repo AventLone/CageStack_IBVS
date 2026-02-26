a9d4f1b1fbb58ed72474ed556ff1cdfc5df5eeca
Avent Lone <piggy.stoic@outlook.com>
2026-02-26 11:30:43 +0800

    :hammer: Update

--- ROS2/packages/perception/include/perception/tools/feature_detect_3d.hpp
+++ ROS2/packages/perception/include/perception/tools/feature_detect_3d.hpp
@@ -2,6 +2,7 @@
 #include <pcl/common/common.h>
 #include <pcl/common/eigen.h>
 #include <pcl/common/centroid.h>
+#include <random>
 
 template<class PointT>
 Eigen::Vector3f getCloudSize(const pcl::PointCloud<PointT>& cloud)
@@ -9,6 +10,95 @@ Eigen::Vector3f getCloudSize(const pcl::PointCloud<PointT>& cloud)
     Eigen::Vector4f min_point, max_point;
     pcl::getMinMax3D(cloud, min_point, max_point);
     return max_point.head<3>() - min_point.head<3>();
+}
+
+template<class PointT>
+float calculateLineAngle(const pcl::PointCloud<PointT>& cloud)
+{
+    // Validate the cloud
+    if (cloud.size() < 2)
+    {
+        throw std::invalid_argument("The number of the points can't be less than 2！");
+    }
+
+    std::vector<cv::Point2f> points;
+    for (const auto& point : cloud.points)
+    {
+        points.emplace_back(point.y, point.x);
+    }
+    cv::Vec4f line; // Fitting result: [vx, vy, x0, y0]; y = k*x + b; k = vy / vx, b = y0 - k*x0
+    cv::fitLine(points, line, cv::DIST_L2, 0.0, 0.01, 0.01);
+    const float angle_rad = -std::atan2(line[1], line[0]);
+    const float angle_deg = angle_rad * 180.0f / M_PIf;
+    return angle_deg;
+}
+
+template<class PointT>
+void getCloud(const pcl::PointCloud<PointT>& src, pcl::PointCloud<PointT>& dst, const std::vector<int>& indices)
+{
+    dst.reserve(indices.size());
+
+    for (const int index : indices)
+    {
+        dst.push_back(src[index]);
+    }
+}
+
+/* Assume by default that this plane is perpendicular to the x-y plane. */
+template<class PointT>
+bool findInliers(const pcl::PointCloud<PointT>& cloud, pcl::PointCloud<PointT>& inliers, const float dist_thresh, const int iters = 300)
+{
+    const int N = cloud.size();
+    if (N < 10)
+    {
+        return false;
+    }
+    std::vector<int> best_inliers_indices;
+
+    std::mt19937 rng(std::random_device{}());
+    std::uniform_int_distribution<int> uni(0, N - 1);
+
+    for (int iter = 0; iter < iters; ++iter)
+    {
+        int i = uni(rng);
+        int j = uni(rng);
+        if (i == j)
+        {
+            continue;
+        }
+        const auto& p1 = cloud[i];
+        const auto& p2 = cloud[j];
+
+        //line in xy-plane: a*x + b*y + d = 0
+        float a = p1.y - p2.y;
+        float b = p2.x - p1.x;
+        float d = p1.x * p2.y - p2.x * p1.y;
+
+        float norm = std::hypot(a, b);
+        if (norm < 1e-6f)
+        {
+            continue;
+        }
+
+        std::vector<int> inliers_indices;
+        for (int k = 0; k < N; ++k)
+        {
+            const auto& p = cloud[k];
+            if (const float dist = std::abs(a * p.x + b * p.y + d) / norm; dist < dist_thresh)
+            {
+                inliers_indices.push_back(k);
+            }
+        }
+
+        if (inliers_indices.size() > best_inliers_indices.size())
+        {
+            best_inliers_indices = std::move(inliers_indices);
+        }
+    }
+
+    getCloud(cloud, inliers, best_inliers_indices);
+
+    return true;
 }
 
 // template<typename PointT>
@@ -186,12 +276,12 @@ Eigen::Vector3f computeCenter(const pcl::PointCloud<PointT>& cloud)
 inline pcl::PointCloud<pcl::PointXYZ> createPalletCloud(const float angle)
 {
     pcl::PointCloud<pcl::PointXYZ> front_face, left_side_face, right_side_face, cloud, angel_cloud;
-    
-    std::random_device rd;   // Random device for seeding
-    std::mt19937 gen(rd());  // Mersenne Twister engine
+
+    std::random_device rd; // Random device for seeding
+    std::mt19937 gen(rd()); // Mersenne Twister engine
 
     std::uniform_real_distribution<float> unifor(0.02, 0.2);
-    std::cauchy_distribution<float> dist_chaos(0.0f, 0.005f);
+    std::cauchy_distribution<float> dist_chaos(0.0f, 0.001f);
 
     for (float y = -0.5f; y < 0.5f; y += 0.01f)
     {
--- ROS2/packages/perception/source/main.cpp
+++ ROS2/packages/perception/source/main.cpp
@@ -25,53 +25,157 @@ class Test : public rclcpp::Node
 public:
     explicit Test() : rclcpp::Node("test"), mProjector(View::TOP, 0.01f)
     {
-        mCloudPub = create_publisher<sensor_msgs::msg::PointCloud2>("test_cloud", rclcpp::SensorDataQoS());
-        mMarkerPub = create_publisher<visualization_msgs::msg::Marker>("marks", rclcpp::SensorDataQoS());
+        // mCloud = std::make_shared<RawCloud>();
+        const auto parallel = create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+        auto sub_options = rclcpp::SubscriptionOptions();
+        sub_options.callback_group = parallel;
+        mCloudSub = create_subscription<sensor_msgs::msg::PointCloud2>("test_cloud", rclcpp::SensorDataQoS(),
+                                                                       [this](const sensor_msgs::msg::PointCloud2::ConstSharedPtr& msg)
+                                                                           {
+                                                                               // RawCloud cloud;
 
-        mTimer = create_wall_timer(std::chrono::milliseconds(200), std::bind(&Test::pubLoop, this));
+                                                                               // 步骤1：循环出队，清空所有旧数据（关键！不依赖size_approx()）
+                                                                               // while (mClouds.try_dequeue(cloud))
+                                                                               // {}
+
+                                                                               // 步骤2：入队新数据
+                                                                               {
+                                                                                   std::lock_guard<std::mutex> lock(mCloudMutex);
+                                                                                   pcl::fromROSMsg(*msg, mCloud);
+                                                                               }
+                                                                               // pcl::fromROSMsg(*msg, cloud);
+                                                                               // if (!mClouds.enqueue(std::move(cloud)))
+                                                                               // {
+                                                                               //     return;
+                                                                               // }
+
+                                                                               // std::unique_lock<std::mutex> lock(mTriggerMutex);
+                                                                               // mIsTriggered = true;
+                                                                               // mTriggerEvent.notify_one();
+                                                                           }, sub_options);
+
+        mCloudPub = create_publisher<sensor_msgs::msg::PointCloud2>("test_cloud", rclcpp::SensorDataQoS());
+        mFrontFacePub = create_publisher<sensor_msgs::msg::PointCloud2>("front_face", rclcpp::SensorDataQoS());
+        mMarkerPub = create_publisher<visualization_msgs::msg::Marker>("marks", rclcpp::SensorDataQoS());
+        mTargetPosePub = create_publisher<geometry_msgs::msg::PoseStamped>("target_pose", rclcpp::ServicesQoS());
+
+        mTimer = create_wall_timer(std::chrono::milliseconds(100), std::bind(&Test::pubLoop, this), parallel);
+        mCloudProcessThread = std::thread(&Test::cloudProcessLoop, this);
+        RCLCPP_INFO(get_logger(), "The node has been activated.");
+    }
+
+    ~Test() override
+    {
+        //
+        // {
+        //     std::unique_lock<std::mutex> lock(mTriggerMutex);
+        //     mIsTriggered = true;
+        //     mTriggerEvent.notify_one();
+        // }
+        if (mCloudProcessThread.joinable())
+        {
+            mCloudProcessThread.join();
+        }
+        RCLCPP_INFO(get_logger(), "The node has been shutdown.");
     }
 
 private:
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr mCloudPub;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr mCloudSub;
+
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr mCloudPub, mFrontFacePub;
     rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr mMarkerPub;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr mTargetPosePub;
 
     rclcpp::TimerBase::SharedPtr mTimer;
     OrthographicProjector<pcl::PointXYZ> mProjector;
+    lockfree_queue<RawCloud> mClouds;
+    RawCloud mCloud;
+    bool mIsTriggered{false};
+    std::mutex mCloudMutex, mTriggerMutex;
+    std::condition_variable mTriggerEvent;
+    std::thread mCloudProcessThread;
 
-
-    void pubLoop()
+    void pubLoop() const
     {
-        const RawCloud test_cloud = createPalletCloud(M_PIf / 6.0f);
+        const RawCloud test_cloud = createPalletCloud(-M_PIf / 6.0f);
         sensor_msgs::msg::PointCloud2 cloud_msg;
         pcl::toROSMsg(test_cloud, cloud_msg);
         cloud_msg.header.frame_id = "map";
         mCloudPub->publish(cloud_msg);
+    }
 
-        mProjector.setCloud(test_cloud.makeShared());
-        const cv::Mat projection = mProjector.projection();
-        if (projection.empty())
+    void cloudProcessLoop()
+    {
+        while (rclcpp::ok())
         {
-            RCLCPP_ERROR(get_logger(), "Cloud projection is empty!");
-            return;
+            // {
+            //     std::unique_lock<std::mutex> lock(mTriggerMutex);
+            //     mTriggerEvent.wait(lock, [this]() -> bool { return mIsTriggered; });
+            // }
+
+            RawCloud::Ptr cloud = std::make_shared<RawCloud>();
+            //
+            {
+                std::lock_guard<std::mutex> lock(mCloudMutex);
+                *cloud = mCloud;
+            }
+            if (cloud->size() < 10)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                continue;
+            }
+
+            mProjector.setCloud(cloud);
+            const cv::Mat projection = mProjector.projection();
+            if (projection.empty())
+            {
+                RCLCPP_ERROR(get_logger(), "Cloud projection is empty!");
+                continue;
+            }
+            cv::Mat opened_img, deisolated_img, denoised_img;
+            filter2d::open(projection, opened_img);
+
+            const auto min_rect = feature2d::detectMinRect(opened_img);
+            filter2d::removeIsolatedPoints(projection, deisolated_img);
+            filter2d::denoise(projection, denoised_img);
+
+            cv::Mat closed_img;
+            filter2d::close(denoised_img, closed_img);
+            cv::Mat edge_img;
+            feature2d::detectEdge(closed_img, edge_img);
+
+            auto line = feature2d::detectRectEdge(denoised_img, feature2d::EdgeType::RIGHT);
+
+            cv::Mat debug_img;
+            cv::cvtColor(projection, debug_img, cv::COLOR_GRAY2BGR);
+            cv::line(debug_img, line.p1, line.p2, cv::Scalar(0, 255, 0), 2);
+
+            cv::Mat mask = cv::Mat::zeros(denoised_img.size(), CV_8UC1);
+            cv::line(mask, line.p1, line.p2, cv::Scalar(255), 40);
+            cv::Mat front_edge;
+            denoised_img.copyTo(front_edge, mask);
+            RawCloud front_edge_cloud = mProjector.extractCloud(front_edge);
+            if (front_edge_cloud.size() < 10)
+            {
+                RCLCPP_ERROR(get_logger(), "front_edge_cloud is too small!");
+                return;
+            }
+            RawCloud front_edge_inlier_cloud;
+            findInliers(front_edge_cloud, front_edge_inlier_cloud, 0.04f);
+
+            Eigen::Vector4f centroid;
+            pcl::compute3DCentroid(front_edge_inlier_cloud, centroid);
+            const float angle = calculateLineAngle(front_edge_inlier_cloud);
+            RCLCPP_INFO(get_logger(), "The angle is %f degrees", angle);
+            RCLCPP_INFO(get_logger(), "The position is (%f, %f)", centroid[0], centroid[1]);
+
+            sensor_msgs::msg::PointCloud2 front_edge_msg;
+            pcl::toROSMsg(front_edge_inlier_cloud, front_edge_msg);
+            front_edge_msg.header.frame_id = "map";
+            mFrontFacePub->publish(front_edge_msg);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(60));
         }
-        cv::Mat opened_img, deisolated_img, denoised_img;
-        filter2d::open(projection, opened_img);
-
-        const auto min_rect = feature2d::detectMinRect(opened_img);
-        filter2d::removeIsolatedPoints(projection, deisolated_img);
-        filter2d::denoise(projection, denoised_img);
-
-        cv::Mat closed_img;
-        filter2d::close(denoised_img, closed_img);
-        cv::Mat edge_img;
-        feature2d::detectEdge(closed_img, edge_img);
-
-        auto line = feature2d::detectRectEdge(opened_img, feature2d::EdgeType::RIGHT);
-        cv::Mat debug_img;
-        cv::cvtColor(projection, debug_img, cv::COLOR_GRAY2BGR);
-        cv::line(debug_img, line.p1, line.p2, cv::Scalar(0, 255, 0), 2);
-
-        RCLCPP_INFO(get_logger(), "Hello");
     }
 };
 
@@ -79,7 +183,11 @@ int main(const int argc, char** argv)
 {
     rclcpp::init(argc, argv);
     const auto node = std::make_shared<Test>();
-    rclcpp::spin(node);
+    // rclcpp::spin(node);
+    rclcpp::executors::MultiThreadedExecutor executor(rclcpp::ExecutorOptions(), 2);
+    executor.add_node(node);
+    executor.spin();
     rclcpp::shutdown();
+
     return 0;
 }
